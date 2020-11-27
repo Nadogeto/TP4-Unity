6c58c089c3e90ef8537f4bd51aac81faea78c24e
Nadjet Tobbal <nadjetobbal@gmail.com>
2020-11-27 19:26:10 +0100

    Update Movement.cs

--- Assets/TP-Voiture/Scripts/Movement.cs
+++ Assets/TP-Voiture/Scripts/Movement.cs
@@ -6,8 +6,8 @@ public class Movement : MonoBehaviour
 {
     public GameObject target;
     public GameObject car;
-
     public float speed = 10;
+
 
     // Update is called once per frame
     void Update()
@@ -15,13 +15,12 @@ public class Movement : MonoBehaviour
         //moves the vehicule to the target position
         if (car.GetComponent<Reservoir>().stop == false)
         {
-            speed = 10;
             transform.position = Vector3.MoveTowards(transform.position, target.transform.position, Time.deltaTime * speed);
         }
-        //when the reservoir has reached 0, stops the car
+        //when the reservoir has reached 0, stops the movements of the car
         if (car.GetComponent<Reservoir>().stop == true)
         {
-            speed = 0;//not the best practice
+           car.GetComponent<Movement>().enabled = false;
         }
     }
 }
